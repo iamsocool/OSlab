76202aefa7ef698ef856d3500692c0e9fd6340ed
iamsocool <18810339332@163.com>
2018-04-04 00:34:49 +0800

    debug

--- lab1/lab1_2_1.c
+++ lab1/lab1_2_1.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<unistd.h>
 int main()
 {
@@ -6,7 +7,7 @@ int main()
   while((pid=fork())<0);
   if(pid==0)
   {
-    if(execlp("./lab1_2_1","lab1_2_1",NULL)<0)
+    if(execlp("./lab1_1_2","lab1_1_2",NULL)<0)
       printf("execlp error\n");
   }
   else
